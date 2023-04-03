74383d32dc23e532df5f8761f182edb34eaa149f
Pobitrodeb <pobitrodn@gmail.com>
2023-04-03 18:41:34 +0600

    table relations done

--- app/Models/Persmission.php
+++ app/Models/Persmission.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Role;
+use App\Models\User;
 
 class Persmission extends Model
 {
     use HasFactory;
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'roles_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_permissions');
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Persmission;
+use App\Models\User;
 
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'slug'
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'roles_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_role');
+    }
+
 }
